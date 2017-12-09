4aa057c7426dae50b31b7e16f6ebe3ea00d711e5
jrdnndtsch <jordanbdeutsch@gmail.com>
2017-12-09 18:49:39 -0500

    change db to pg and start on selected post listings page

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171208140630) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "grams", force: :cascade do |t|
     t.string   "image_url"
     t.string   "link"
@@ -23,7 +26,7 @@ ActiveRecord::Schema.define(version: 20171208140630) do
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
     t.boolean  "selected",          default: false
-    t.index ["user_id"], name: "index_grams_on_user_id"
+    t.index ["user_id"], name: "index_grams_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,8 +44,8 @@ ActiveRecord::Schema.define(version: 20171208140630) do
     t.datetime "updated_at",                          null: false
     t.string   "insta_client_id"
     t.string   "insta_client_token"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
