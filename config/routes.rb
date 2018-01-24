185321cd4cf13e60fcebd239f23fb49cfbd6a9a2
jrdnndtsch <jordanbdeutsch@gmail.com>
2018-01-24 16:39:43 +0100

    route home to index for grams

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :grams
   devise_for :users
-  root 'pages#home'
+  root 'grams#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
   get '/import', to: 'grams#import'
   post '/grams/filter_grams', to: 'grams#filter'
